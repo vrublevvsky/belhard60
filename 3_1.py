812e0a958f54c095caa14e2e3a6b0155795fb2ab
vrublevvsky <giaxigah@gmail.com>
2022-09-08 15:13:49 +0300

    homework 3_1 (100%) rdy

--- 3_1.py
+++ 3_1.py
@@ -1,7 +1,7 @@
 #Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 
 sentence=input()
-print(sentence)
+print(sentence.replace(' ', '-'))
 sentence=sentence.split(' ')
 sentence={'-'.join(sentence)}
 print(sentence)

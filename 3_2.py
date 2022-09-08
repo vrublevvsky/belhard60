f7143449faceba226326d923481f6feea1359c9f
vrublevvsky <giaxigah@gmail.com>
2022-09-08 14:28:04 +0300

    homework 3_2 (100%) rdy

---
+++ 3_2.py
@@ -0,0 +1,7 @@
+# Пользователь вводит 3 числа, найти среднее арифмитическое с точность 3
+
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = (num1 + num2 + num3) / 3
+print(round(num4, 3))

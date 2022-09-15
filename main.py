26d7bf40811b57e57512cfa2f3ad410f056830ef
Artem <energiex@mail.ru>
2022-09-15 21:29:56 +0300

    Revert "Update main.py"
    This reverts commit af2a4797

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print('hello world')
-print('hello python')
+from random import randint
+a = randint(1, 30)
+b = int(input())
+y = 5
+while not a == b:
+    print("не угадал")
+    print('осталось попыток')
+    y = y - 1
+    print(y)
+    if a > b:
+        print('число больше')
+    else:
+        print('число меньше')
+    b = int(input())
+print('верно')
